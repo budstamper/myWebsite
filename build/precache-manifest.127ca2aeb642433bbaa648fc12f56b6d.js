a03128e33c2415995ecd3d4d6abf07fc6e306739
Bud <40903874+budstamper@users.noreply.github.com>
2019-12-06 13:39:30 -0500

    build folder

---
+++ build/precache-manifest.127ca2aeb642433bbaa648fc12f56b6d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c18b61341aa0419ef8dac8c1322ac7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39f6c4eb6ab7f29908a7",
+    "url": "./static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "c7938dc8d44818ca4044",
+    "url": "./static/css/main.7b28a1c1.chunk.css"
+  },
+  {
+    "revision": "39f6c4eb6ab7f29908a7",
+    "url": "./static/js/2.1bc658ae.chunk.js"
+  },
+  {
+    "revision": "c7938dc8d44818ca4044",
+    "url": "./static/js/main.2b658a84.chunk.js"
+  },
+  {
+    "revision": "dd22929369d137db1959",
+    "url": "./static/js/runtime-main.dbd7deed.js"
+  },
+  {
+    "revision": "d73e5c5efff85b3dfba742d694123a5c",
+    "url": "./static/media/Alan-Sandra.d73e5c5e.jpg"
+  },
+  {
+    "revision": "1d056fc2d689382b57ff7eed863274f5",
+    "url": "./static/media/Instagram-Logo.1d056fc2.png"
+  },
+  {
+    "revision": "9ed9d62bd13f484a3206444b99bd379c",
+    "url": "./static/media/chairs.9ed9d62b.png"
+  },
+  {
+    "revision": "faf2a29305a70ad77040e1a20a6d7490",
+    "url": "./static/media/couch.faf2a293.png"
+  },
+  {
+    "revision": "5764fce9cfb9060a442d7c2813bc3c94",
+    "url": "./static/media/couch2.5764fce9.png"
+  },
+  {
+    "revision": "c7768de1f16e311961513737f2fca900",
+    "url": "./static/media/couch4.c7768de1.png"
+  },
+  {
+    "revision": "3dfa0490b36a5d4ee393803167873fe4",
+    "url": "./static/media/logo-transparent.3dfa0490.png"
+  },
+  {
+    "revision": "02566b06e4fc28790c55edcb72cec455",
+    "url": "./static/media/logo.02566b06.png"
+  },
+  {
+    "revision": "beec1fde91a9d27bb2999c4b229f287c",
+    "url": "./static/media/mattress.beec1fde.png"
+  },
+  {
+    "revision": "1909326c7a3b8df34d3ff44ffd4c8e8a",
+    "url": "./static/media/reveal.1909326c.png"
+  }
+]);
